8057fec25500e0010c0d5f6325b713c43d54d3c6
Bikash Sah <51731962+BikashSah999@users.noreply.github.com>
2023-10-27 00:28:35 +0545

    chore: update title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,16 @@ import client from '@/apollo/apollo';
 import '@/styles/globals.css';
 import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
+      <Head>
+        <title>
+          Lawpath - Aus Address Validation
+        </title>
+      </Head>
       <Component {...pageProps} />
     </ApolloProvider>
   );
